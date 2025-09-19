4cb2a427dfdfcdaca4211247035d371a4d03e88c
miiiso1983 <info.skylimitx@gmail.com>
2025-09-19 23:56:13 +0300

    Sitemap: render XML declaration safely in Blade; move content-type header to controller (already set). Also clear compiled view locally.

--- resources/views/public/sitemap/xml.blade.php
+++ resources/views/public/sitemap/xml.blade.php
@@ -1,7 +1,4 @@
-@php
-    header('Content-Type: application/xml; charset=utf-8');
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
-@endphp
+{!! '<'.'?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($urls as $u)
   <url>
