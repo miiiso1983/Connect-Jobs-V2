fb7001eec12e3925644e1c8751cc81d8080e2ad2
miiiso1983 <info.skylimitx@gmail.com>
2026-02-03 00:39:12 +0300

    يسبسيبسيب

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-base-content/80 hover:bg-base-200 focus:outline-none focus:bg-base-200 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-200 font-medium hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-600 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
