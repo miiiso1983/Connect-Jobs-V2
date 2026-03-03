52f4a9bb3594503e18429a2838e9f5c7939e695c
miiiso1983 <info.skylimitx@gmail.com>
2026-03-03 04:52:43 +0300

    ثقثصفا

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,8 @@
-<img src="{{ asset('images/brand/logo.png') }}" alt="Connect Job" {{ $attributes->merge(['class' => 'h-10 w-auto']) }}>
+@php
+    // Prefer the horizontal web logo when available; fallback to the square logo.
+    $logoSrc = file_exists(public_path('images/brand/logo-horizontal.png'))
+        ? asset('images/brand/logo-horizontal.png')
+        : asset('images/brand/logo.png');
+@endphp
+
+<img src="{{ $logoSrc }}" alt="Connect Jobs" {{ $attributes->merge(['class' => 'h-10 w-auto object-contain']) }}>
